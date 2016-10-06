aa0f27c53c285235b927ed072c348e2d37310ff9
Chris Meyers <Chris@NullRecursion.com>
2016-10-05 20:48:31 -0700

    add newly required clientId

--- streamFuncs.py
+++ streamFuncs.py
@@ -1,10 +1,11 @@
 import httplib
 import json
+clientId = 'opposq941af1ooy9hwob1gud9ixogm0'
 
 def getTwitchStreams():
   conn = httplib.HTTPSConnection('api.twitch.tv')
   conn.connect()
-  request = conn.putrequest('GET', '/kraken/streams?game=StarCraft+II')
+  request = conn.putrequest('GET', '/kraken/streams?game=StarCraft+II&client_id=' + clientId)
   conn.putheader('Content-Type','application/json')
   conn.endheaders()
   conn.send('')
